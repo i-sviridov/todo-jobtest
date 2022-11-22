c12fd271e63b3fcd76f83fa4df3bffff412335b4
Ilya Sviridov <ilya.sviridov@yandex.ru>
2022-11-22 20:16:10 +0300

    initial deploy

---
+++ components/InputField.js
@@ -0,0 +1,13 @@
+export default function InputField() {
+  return (
+    <form>
+      <input
+        type="text"
+        id="input"
+        name="input-field"
+        value="Enter your task"
+      />
+      <button>Click</button>
+    </form>
+  );
+}
