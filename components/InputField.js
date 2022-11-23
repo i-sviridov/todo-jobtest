7fb1c59091f83ba22a63b605782d99c8591e689b
Ilya Sviridov <ilya.sviridov@yandex.ru>
2022-11-23 09:38:13 +0300

    input field logic added

--- components/InputField.js
+++ components/InputField.js
@@ -1,11 +1,31 @@
+import { useState } from 'react';
+
 export default function InputField() {
+  const [userInput, setUserInput] = useState('Enter your task');
+  console.log(userInput);
+
+  function userInputHandler(event) {
+    setUserInput(event.target.value);
+  }
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+    fetch('https://todo-app-f2649-default-rtdb.firebaseio.com/tasks.json', {
+      method: 'POST',
+      body: JSON.stringify({
+        task: userInput,
+      }),
+    }).then(() => setUserInput('Enter your task'));
+  }
+
   return (
-    <form>
+    <form onSubmit={submitFormHandler}>
       <input
         type="text"
         id="input"
         name="input-field"
-        value="Enter your task"
+        value={userInput}
+        onChange={userInputHandler}
       />
       <button>Click</button>
     </form>
